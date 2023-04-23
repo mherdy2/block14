a6b9edb8f18040e83650bb3c0bae67bd64a1b566
Maui Bautista <mherdy_2@hotmail.com>
2023-04-23 12:42:30 -0700

    fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,12 @@
-function char_count(str, letter) {
-  var letter_Count = 0;
-  for (var position = 0; position < str.length; position++) {
-    if (str.charAt(position) == letter) {
-      letter_Count += 1;
-    }
+const n = 100;
+for (let x = 1; x <= n; x++) {
+  if (x % 3 === 0 && x % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (x % 5 === 0) {
+    console.log("Buzz");
+  } else if (x % 3 === 0) {
+    console.log("Fizz");
+  } else {
+    console.log(x);
   }
-  return letter_Count;
 }
-
-console.log(char_count("w3resource.com", "o"));
