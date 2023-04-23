eac2e2f143d57b42fe68518e4e8a3b0fe4af2c84
Maui Bautista <mherdy_2@hotmail.com>
2023-04-23 10:13:26 -0700

    reverseArray

---
+++ reverseArray.js
@@ -0,0 +1,7 @@
+//this is a function that reverses the order of an array
+//and prints the reversed array.
+const array1 = [1, -1, 2, -3, 5, -8, 13];
+// input array
+for (let i = array1.length; i >= 1; i--) {
+  console.log(array1[i - 1]);
+}
