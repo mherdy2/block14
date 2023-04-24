8d3389b0a27bfe08f45dc3c01740b1316792392d
Maui Bautista <mherdy_2@hotmail.com>
2023-04-24 14:10:12 -0700

    additional commits

--- vow-vs-cons.js
+++ vow-vs-cons.js
@@ -7,40 +7,32 @@ function go() {
   // count the number of vowels and consonants in a input string
   // return the number of vowels and consonants in a input string
   // const inputWord = "the quick brown fox jumps over the lazy dog";
-  const vowelArray = "aeiou",
-    consonantArray = "bcdfghjklmnpqrstvwxyz";
+  const vowelArray = "AEIOUaeiou";
   var vowel = [],
     consonant = [];
-  //   this function will count the number of vowels in the input string
+  //  this loop function will count the number of vowels
+  // in the inputWord and search if it is in the vowelArray
   for (let x = 0; x < inputWord.length; x++) {
     for (let y = 0; y < vowelArray.length; y++) {
-      // comparison of vowels from the input string with the vowel array
+      // comparison of vowels from the inputWord with the vowelArray
+      // then store it in the vowelArray
       if (inputWord[x] === vowelArray[y]) {
         // counter for vowels
         vowel++;
       }
+      // function to count the number
+      //  in the inputWord minus the number of vowels
+      // will be the number of consonants
+      consonant = inputWord.length - vowel;
+      // returns the result on gathered count for variables "consonant", "vowel to p1.
+      document.getElementById("p1").innerHTML =
+        inputWord +
+        " has " +
+        consonant +
+        " consonant" +
+        "  and " +
+        vowel +
+        " vowel";
     }
   }
-  // this function will count the number of consonants in the input string
-  for (let x = 0; x < inputWord.length; x++) {
-    for (let z = 0; z < consonantArray.length; z++) {
-      // comparison of consonants from the input string with the consonant array
-      if (inputWord[x] === consonantArray[z]) {
-        // counter for consonants
-        consonant++;
-      }
-    }
-  }
-  // prints the result on gathered count for each variable
-  document.getElementById("p1").innerHTML =
-    inputWord +
-    " has " +
-    consonant +
-    " consonant" +
-    "  and " +
-    vowel +
-    " vowel";
 }
-// console.log(
-//   inputWord + " has " + consonant + " consonant" + "  and " + vowel + " vowel"
-// );
